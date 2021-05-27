e87319f55b7157ee1bf8f40093ab583af5fa7143
Prince0297Ahirwar <princeahirwar51@gmail.com>
2021-05-27 09:10:48 +0530

    add directory structure,home controller,routes,set vie engine and folder

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+const port = 8000;
+
+//setting up ejs engine and path
+
+app.set("view engine","ejs");
+app.set("views",'./views');
+
+
+//routing to routes
+
+app.use('/',require('./routes'));
+
+
+//setting static files for app
+
+app.use(express.static('assets'));
+
+app.listen(port,function(err){
+    if(err){
+        console.log(`Error in starting server ${err} `);
+        return;
+    }
+    console.log(`Server started on port: ${port}`);
+});
